4763622337231091fce4504e55977b7bb9e3289d
cfegley <103463191+cfegley@users.noreply.github.com>
2022-04-21 10:30:19 -0700

    Add files via upload

---
+++ printSum.py
@@ -0,0 +1,19 @@
+def printSumDifferenceProduct():
+
+	num1 = float(3)
+	num2 = float(5)
+
+	num3 = int(8)
+	num4 = int(6)
+
+	num5 = float(10.0)
+	num6 = int(5)
+
+	print("The sum of 3.0 and 5.0 = ",num1 + num2 ,type(num1 + num2))
+
+	print("The difference of 8 - 6 = ",num3 - num4, type(num1 - num2))
+
+	print("The product of 10.0 x 5 = ",num5 * num6, type(num5 * num6))
+
+
+printSumDifferenceProduct()
